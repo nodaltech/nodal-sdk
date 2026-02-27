657bd504831265d88ad1f0d0249abe0d3dea4234
arkowitz <arkowitz@gmail.com>
2026-02-27 16:39:45 -0500

    option to specify inbound destination port in blocks

--- examples/aws_acl.py
+++ examples/aws_acl.py
@@ -49,6 +49,9 @@ async def main():
             fout.write(
                 'AWS_SECRET_ACCESS_KEY: ""  # Replace with your AWS secret key\n'
             )
+            fout.write(
+                'SPECIFY_INBOUND_PORT: false  # Set true to block specific internal port TESTING ONLY\n'
+            )
         print("wrote config file " + cf + " in local dir, please edit it")
         exit(1)
 
@@ -88,6 +91,14 @@ async def main():
                         ):
                             rule_number = entry["RuleNumber"] + 1
 
+# {'mitigation_id': '7616049587240934162', 'mitigator': '*', 'targets': {'IntExtIp': {'int_ip': '10.0.1.12', 'int_ports': [32768], 'ext_ip': '185.125.188.57', 'ext_ports': [443]}}, 'status': 'Requested', 'tag': 'Short', 'case_id': 'f675ae35-af93-4bff-b8f3-9f1b6fae7fb2', 'ts': 1772227513.364715, 'expiry': 1772227813.364715}
+
+                ingress_port_range = {"From": 0, "To": 65535}
+                if conf["SPECIFY_INBOUND_PORT"]:
+                    inport = target["int_ports"][0]
+                    ingress_port_range = {"From": inport, "To": inport}
+                    print("set ingress port to " + str(inport))
+
                 # Add inbound deny rule
                 ec2.create_network_acl_entry(
                     NetworkAclId=conf["AWS_NETWORK_ACL_ID"],
@@ -96,7 +107,7 @@ async def main():
                     RuleAction="deny",
                     Egress=False,
                     CidrBlock=f"{ext_ip}/32",
-                    PortRange={"From": 0, "To": 65535},
+                    PortRange=ingress_port_range,
                 )
 
                 # Add outbound deny rule
